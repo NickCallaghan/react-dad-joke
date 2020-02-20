4615f515eb943953db508897a9514e94e59293bb
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-20 23:43:55 +0000

    adding vote color basic

--- src/Vote.js
+++ src/Vote.js
@@ -2,13 +2,34 @@ import React, { Component } from "react";
 import "./Vote.scss";
 
 export default class Vote extends Component {
+  constructor(props) {
+    super(props);
+    this.colors = {
+      "-3": "#F0594E",
+      "-2": "#F07872",
+      "-1": "#F0A29D",
+      "0": "#F0D58B",
+      "1": "#E3FFE6",
+      "2": "#96FFA3",
+      "3": "#79CC83",
+      "4": "#348054"
+    };
+    this.style = { backgroundColor: "#F0D58B" };
+  }
+
   handleVote = voteAdjustment => {
     this.props.voteOnJoke(this.props.jokeId, voteAdjustment);
   };
 
+  determineColor = () => {
+    console.log("colors");
+  };
+
+  componentDidMount() {}
+
   render() {
     return (
-      <div className="vote">
+      <div className="vote" style={this.style}>
         <div className="vote-down" onClick={() => this.handleVote(-1)}>
           -
         </div>
