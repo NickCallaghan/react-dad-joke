34c2ddb52b86ac4b9c29b28df4663aab1d543cb5
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-21 16:05:01 +0000

    Updates

---
+++ precache-manifest.d3ce4f5b35ba513d8db888b250459dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5f5573c05f692b355e0f441006482a",
+    "url": "/react-dad-joke/index.html"
+  },
+  {
+    "revision": "d9d5e3447ffa48b2be2e",
+    "url": "/react-dad-joke/static/css/main.c3283df4.chunk.css"
+  },
+  {
+    "revision": "772d9276d294c37786d6",
+    "url": "/react-dad-joke/static/js/2.a47ec0e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-dad-joke/static/js/2.a47ec0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d5e3447ffa48b2be2e",
+    "url": "/react-dad-joke/static/js/main.9c7be67a.chunk.js"
+  },
+  {
+    "revision": "c8c8066c15f319096767",
+    "url": "/react-dad-joke/static/js/runtime-main.f008c801.js"
+  },
+  {
+    "revision": "6fd227e4b6dc0704d760f5e893c2149e",
+    "url": "/react-dad-joke/static/media/laugh.6fd227e4.svg"
+  }
+]);
